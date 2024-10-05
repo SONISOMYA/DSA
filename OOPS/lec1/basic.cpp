ea720111dc2d0a61eb28bd0240ba6155aa2b8409
Somya Soni <somyasoni0411@gmail.com>
2024-10-05 11:27:26 +0530

    first commit

---
+++ OOPS/lec1/basic.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    //properties or states
+    int age;
+    string name;
+
+    //behaviour
+
+    void bark(){
+    }
+    void eat(){
+    } 
+};
+
+int main(){
+    //object creation 
+
+//static
+Animal Ramesh;
+Ramesh.age;
+
+
+//dynamiv
+}
