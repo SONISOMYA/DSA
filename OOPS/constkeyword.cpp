38b34f9da23306162979aaae15cc00247fd26764
Somya Soni <somyasoni0411@gmail.com>
2024-10-06 18:42:25 +0530

    constddataconstpointer

--- OOPS/constkeyword.cpp
+++ OOPS/constkeyword.cpp
@@ -9,17 +9,39 @@ int main(){
 
 //CONST WITH POINTERS
 
-const int *a= new int(2 ); //const data , no const pointer
-int const *a = new int(2); // same as the above line
+// const int *a= new int(2 ); //const data , no const pointer
+// int const *a = new int(2); // same as the above line
 // *a=2; cant change the content of the pointer 
+// cout<<*a<<endl;
+// int b= 20;
+// a= &b; //* but the pointer it self can be reassigned 
+// cout<<*a<<endl;
+
+
+
+//CONSTANT POINTER BUT NON CONSTANT DATA
+//  int *const  a= new int(2);
+//  cout<<*a<<endl;
+//  *a=20; // chal jayega
+//  cout<<*a<<endl;
+//  int b= 20;
+//  a=&b; //nahi chalega
+
+
+
+
+//const pointer , const data
+
+const int *const a= new int (2);
 cout<<*a<<endl;
-int b= 20;
-a= &b; //* but the pointer it self can be reassigned 
-cout<<*a<<endl;
+*a = 50;
+int b=100;
+a= &b;
 
 
 
 
-    return 0;
+
+return 0;
 
 }
