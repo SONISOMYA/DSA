a27615dc9a9b60d766f59c27a6d63789d3dd31cd
Somya Soni <133803930+SONISOMYA@users.noreply.github.com>
2025-05-25 23:20:44 +0530

    Time: 35 ms (40.29%), Space: 14.1 MB (63.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -10,7 +10,7 @@ public:
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return {i, j};
+                    return {i,j };
                 }
             }
         }
