35ffa294512540561fb6b589d3606dc526b42451
Somya Soni <133803930+SONISOMYA@users.noreply.github.com>
2025-08-03 19:23:15 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.61%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int sqsum(int n ){
+        int sum = 0;
+        while(n>0){
+         int digit = n%10;
+         sum+=digit*digit;
+         n/=10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        int slow= n;
+        int fast = n;
+
+        do{
+            slow=sqsum(slow);
+            fast=sqsum(sqsum(fast));
+
+        }while(slow!=fast);
+
+
+        return slow==1;
+    }
+    
+};
