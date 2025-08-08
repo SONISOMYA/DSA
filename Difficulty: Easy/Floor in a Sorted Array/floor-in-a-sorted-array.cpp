166ce46383a2413d633b85ed9105328631ffee19
Somya Soni <133803930+SONISOMYA@users.noreply.github.com>
2025-08-08 14:36:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
@@ -2,6 +2,20 @@ class Solution {
   public:
     int findFloor(vector<int>& arr, int x) {
         // code here
+        int s=0;
+        int e=arr.size()-1;
+        int ans=-1;
         
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]<=x){
+                ans=mid;
+                s=mid+1;
+            }
+            else
+            e=mid-1;
+        }
+      
+        return ans ;
     }
 };
